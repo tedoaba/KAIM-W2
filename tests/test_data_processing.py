e4a81d7073752697cf2cdc4dda9577f8d75d37b7
tedoaba <tedoaba@gmail.com>
2024-09-09 11:34:42 +0300

    Test Data Processing

--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -1,5 +1,8 @@
 import pytest
 import pandas as pd
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.data_processing import load_and_preprocess_data
 
 def test_load_and_preprocess_data():
