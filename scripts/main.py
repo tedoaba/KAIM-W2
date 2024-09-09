948227d9e8350a8ed89c61e7da06eed93f06241f
tedoaba <tedoaba@gmail.com>
2024-09-09 11:33:13 +0300

    Main

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,14 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pandas as pd
 from src.data_processing import load_and_preprocess_data
 from src.analysis import aggregate_experience_data
 from src.clustering import perform_clustering
 
 # Load and preprocess data
-df = load_and_preprocess_data('telecom_dataset.csv')
+df = load_and_preprocess_data('../data/data.csv')
 
 # Aggregate experience data
 experience_data = aggregate_experience_data(df)
