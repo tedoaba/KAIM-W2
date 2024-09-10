dd9c0e835cfcf11938e975fc511df46d14883ce4
tedoaba <tedoaba@gmail.com>
2024-09-10 12:28:33 +0300

    test modified

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -2,7 +2,6 @@ import pytest
 from src.data_loader import load_data
 
 def test_load_data():
-    path = 'tests/test_data.csv'
+    path = '../data/data.csv'
     df = load_data(path)
     assert not df.empty
-    assert list(df.columns) == ['MSISDN/Number', 'Dur. (ms)', 'Total DL (Bytes)', 'Total UL (Bytes)']
